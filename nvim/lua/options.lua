abed455d71bb8576743676eae0dbe608a25fbafe
Dmytro Oleynik <dmytrooliinyk2803@gmail.com>
2026-01-24 15:47:21 +0000

    This is last change in colors. I promise!

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -11,6 +11,7 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.guicursor = ""
+vim.o.colorcolumn = "80"
 
 vim.o.ignorecase = true
 vim.o.smartcase = true
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,15 @@
 return {
 	{
-		"rose-pine/neovim",
-		name = "rose-pine",
+		"projekt0n/github-nvim-theme",
+		name = "github-theme",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			require("rose-pine").setup({})
-			-- vim.cmd("colorscheme rose-pine-main")
-		end,
-	},
-	{
-		"shaunsingh/nord.nvim",
-		opts = {
-			transparent = true,
-		},
-		config = function()
-			-- vim.g.nord_italic = false
-			-- vim.g.nord_bold = false
-			vim.cmd([[colorscheme nord]])
+			require("github-theme").setup({
+				-- ...
+			})
 
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-			vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-			vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
-
-			vim.opt.colorcolumn = "80"
+			vim.cmd("colorscheme github_dark_default")
 		end,
 	},
 }
