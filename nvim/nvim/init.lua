410fa2f0442b131cdd6083be3f43c6e00804ff1a
Dmytro Oleynik <dmytrooliinyk2803@gmail.com>
2026-03-24 21:23:06 +0000

    remove unnecessary plugins

---
+++ nvim/nvim/init.lua
@@ -0,0 +1,9 @@
+require("options")
+require("keymaps")
+require("autocmds")
+
+-- Instal plugin manager (config/lazy.lua)
+require("config.lazy")
+
+-- Load all plugins from lua/plugins
+require("lazy").setup("plugins")
