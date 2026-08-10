01c329d31b9ddd61f9d09344a3bdd5607cd809e2
Dmytro Oleynik <dmytrooliinyk2803@gmail.com>
2026-08-11 00:02:57 +0100

    stable and nice with grubber darker

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,23 +1,102 @@
-require("options")
+-- Options
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.g.mapleader = " "
+vim.opt.wrap = false
+vim.opt.signcolumn = "yes"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.list = true
+vim.opt.scrolloff = 10
+vim.opt.confirm = true
+vim.opt.langmap = "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz"
+vim.g.netrw_banner = 0
 
--- Setup plugin manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		error("Error cloning lazy.nvim:\n" .. out)
-	end
+-- Keymaps
+vim.keymap.set("n", "-", "<cmd>Ex<CR>")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("v", "\\p", '"_dP')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
+vim.keymap.set({ "n", "v" }, "<leader>d", '"+d')
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p')
+
+-- Colortheme
+vim.pack.add({"https://github.com/EdenEast/nightfox.nvim" })
+vim.cmd.colorscheme("carbonfox")
+
+-- Telescope 
+vim.pack.add({
+    { src = "https://github.com/nvim-lua/plenary.nvim" },
+    { src = "https://github.com/nvim-telescope/telescope.nvim" }
+})
+require("telescope").setup({})
+
+vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
+vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep)
+vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers)
+
+-- Fugitive (git)
+vim.pack.add({"https://github.com/tpope/vim-fugitive"})
+vim.keymap.set("n", "<leader>gs", "<cmd>Git<CR>")
+
+-- TreeSitter
+do
+    vim.pack.add({"https://github.com/nvim-treesitter/nvim-treesitter"})
+
+    require('nvim-treesitter').setup({
+      -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+      install_dir = vim.fn.stdpath('data') .. '/site'
+    })
+
+    parsers = { 'rust', 'javascript', 'python', 'c', 'bash' }
+    require('nvim-treesitter').install(parsers)
+
+    ---@param buf integer
+    ---@param language string
+    local function treesitter_try_attach(buf, language)
+        -- Check if a parser exists and load it
+        if not vim.treesitter.language.add(language) then return end
+        -- Enable syntax highlighting and other treesitter features
+        vim.treesitter.start(buf, language)
+
+        -- Enable treesitter based folds
+        -- For more info on folds see `:help folds`
+        -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        -- vim.wo.foldmethod = 'expr'
+
+        -- Check if treesitter indentation is available for this language, and if so enable it
+        -- in case there is no indent query, the indentexpr will fallback to the vim's built in one
+        local has_indent_query = vim.treesitter.query.get(language, 'indents') ~= nil
+
+        -- Enable treesitter based indentation
+        if has_indent_query then vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" end
+    end
+
+    local available_parsers = require('nvim-treesitter').get_available()
+    vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+            local buf, filetype = args.buf, args.match
+
+            local language = vim.treesitter.language.get_lang(filetype)
+            if not language then return end
+
+            local installed_parsers = require('nvim-treesitter').get_installed 'parsers'
+
+            if vim.tbl_contains(installed_parsers, language) then
+                -- Enable the parser if it is already installed
+                treesitter_try_attach(buf, language)
+            elseif vim.tbl_contains(available_parsers, language) then
+                -- If a parser is available in `nvim-treesitter`, auto-install it and enable it after the installation is done
+                require('nvim-treesitter').install(language):await(function() treesitter_try_attach(buf, language) end)
+            else
+                -- Try to enable treesitter features in case the parser exists but is not available from `nvim-treesitter`
+                treesitter_try_attach(buf, language)
+            end
+        end,
+    })
 end
-
----@type vim.Option
-local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
-
--- Load all plugins from lua/plugins
-require("lazy").setup("config", { change_detection = { notify = false } })
-
--- Other modules 
-require("keymaps")
-require("autocmds")
-
